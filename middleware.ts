612b21fd4e001478d95283bdda58783b5e443575
CzechCoder <burian.tom@hotmail.com>
2024-01-22 11:56:39 +0100

    fix(master) Fix middleware login

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,10 @@ import { NextResponse } from "next/server";
 export default function middleware(req: any) {
   let verify = req.cookies.get("next-auth.session-token");
   let url = req.url;
+  const loginLocal = "http://localhost:3000/login";
+  const loginVercel = "https://mclaren-store-dashboard.vercel.app/login";
 
   if (!verify && url.includes("/dashboard")) {
-    return NextResponse.redirect("http://localhost:3000/login");
+    return NextResponse.redirect(loginVercel);
   }
 }
