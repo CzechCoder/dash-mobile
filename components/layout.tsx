378da41b09d2ea21cf454d7462e40e61fa78207a
CzechCoder <burian.tom@hotmail.com>
2024-01-22 19:52:31 +0100

    feat(master) layout metadata

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,11 @@
 import Sidebar from "@/components/sidebar";
+import { Metadata } from "next";
 import { usePathname } from "next/navigation";
 import { ReactElement } from "react";
+
+export const metadata: Metadata = {
+  title: "De main page",
+};
 
 const Layout = ({ children }: { children: ReactElement }) => {
   const pathname = usePathname();
