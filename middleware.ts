b580bdb420c06d29fadd5603fd607b3d50647446
CzechCoder <burian.tom@hotmail.com>
2024-01-22 14:21:30 +0100

    fix(master) Change middleware verify

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 
 export default function middleware(req: any) {
-  let verify = req.cookies.get("next-auth.session-token");
+  let verifyLocal = req.cookies.get("next-auth.session-token");
+  let verify = req.cookies.get("__Secure-next-auth.session-token");
   let url = req.url;
   const loginLocal = "http://localhost:3000/login";
   const loginVercel = "https://mclaren-store-dashboard.vercel.app/login";
