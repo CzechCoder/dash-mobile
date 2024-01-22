a60754af4b3d93839b2ec86c93daba51e1d0a171
CzechCoder <burian.tom@hotmail.com>
2024-01-22 18:25:20 +0100

    fix(mater) Change img link address

--- data/products.ts
+++ data/products.ts
@@ -10,18 +10,18 @@ export const PRODUCTS: Products[] = [
     id: 1,
     name: "Formula 1 car",
     stock: 124,
-    img: "http:/tomasburian.com/dev/cdn/mclaren-store/01.jpg",
+    img: "http://tomasburian.com/dev/cdn/mclaren-store/01.jpg",
   },
   {
     id: 2,
     name: "Baseball cap",
     stock: 250,
-    img: "http:/tomasburian.com/dev/cdn/mclaren-store/02.jpg",
+    img: "http://tomasburian.com/dev/cdn/mclaren-store/02.jpg",
   },
   {
     id: 3,
     name: "F1 Team orange t-shirt",
     stock: 352,
-    img: "http:/tomasburian.com/dev/cdn/mclaren-store/03.jpg",
+    img: "http://tomasburian.com/dev/cdn/mclaren-store/03.jpg",
   },
 ];
