bc78d33a89dfa253a56ee929ba20ca70bba62236
CzechCoder <burian.tom@hotmail.com>
2024-01-24 11:18:23 +0100

    fix(master) remove some useless code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,7 @@ import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
